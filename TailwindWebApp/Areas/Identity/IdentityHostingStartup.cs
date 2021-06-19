5cea345bed3b22a25076e6ca972914dbd8a59176
Greg Lahaye <greg@lahayes.net>
2021-06-19 20:28:02 +0800

    Scaffold Identity area

---
+++ TailwindWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TailwindWebApp.Data;
+using TailwindWebApp.Models;
+
+[assembly: HostingStartup(typeof(TailwindWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace TailwindWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
